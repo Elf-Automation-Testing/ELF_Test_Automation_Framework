f756fa7dd7faf4c837567ac7a9df2e300d87ecd5
abhis <abhis@Abhijith>
2023-03-23 10:18:21 +0530

    Modified Base_Page and Added TroubleTickets_Page

--- ELF_Base_Framework/src/test/java/testCaseSample/TC_SampleTestCase_Test.java
+++ ELF_Base_Framework/src/test/java/testCaseSample/TC_SampleTestCase_Test.java
@@ -1,14 +1,14 @@
 package testCaseSample;
 
 import org.automation.base.Base_Test;
+import org.automation.element_repository.TroubleTickets_Page;
 import org.testng.annotations.Test;
-
-
 public class TC_SampleTestCase_Test extends Base_Test{
 	
 	@Test
 	public void sample() throws InterruptedException {
 		homePage.getCustomerProfileLink(emailId).click();
+		TroubleTickets_Page trouble=new TroubleTickets_Page(driver);
+	
 	}
-
 }
